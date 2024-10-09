142cb0caad05a3ed285d93114723876d3241b24b
IlefRjiba <ilef.rjiba@insat.ucar.tn>
2024-10-09 23:08:09 +0200

    consultation des mangashelves

---
+++ src/Controller/MangashelfController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Mangashelf;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\Persistence\ManagerRegistry;
+
+
+class MangashelfController extends AbstractController
+{
+    
+    #[Route('/mangashelf', name: 'app_mangashelf', methods:['GET'] )]
+    public function listAction(ManagerRegistry $doctrine): Response
+    {
+        $entityManager= $doctrine->getManager();
+        $mangashelfs = $entityManager->getRepository(Mangashelf::class)->findAll();
+
+        return $this->render('mangashelf/index.html.twig', [
+            'controller_name' => 'MangashelfController',
+            "mangashelves" => $mangashelfs
+        ]);
+    }
+
+
+    /**
+     * @param Integer $id
+     */
+    #[Route('/mangashelf/{id}', name: 'mangashelf_show', requirements: ['id' => '\d+'])]
+    public function show(ManagerRegistry $doctrine, $id) : Response
+    {
+            $mangashelfRepo = $doctrine->getRepository(mangashelf::class);
+            $mangashelf = $mangashelfRepo->find($id);
+
+            if (!$mangashelf) {
+                    throw $this->createNotFoundException('The mangashelf does not exist');
+            }
+
+            return $this->render('mangashelf/show.html.twig',
+            [ 'mangashelf' => $mangashelf ]);
+
+    }
+
+}
