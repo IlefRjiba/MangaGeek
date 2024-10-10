de736eeea2c853f52babdf1bb4da3d38e3f2cda1
IlefRjiba <ilef.rjiba@insat.ucar.tn>
2024-10-10 22:52:55 +0200

    Readme.md update

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 Nom et Prénom: Rjiba Ilef
 
-Ce site web, réalisé par symfony, est une interface qui permet aux collectionneurs de mangas de mettre leurs mangas préférés en valeur, en les partageant avec d'autres utilisateurs, les ajoutant dans leur collection. 
+Ce site web, réalisé par symfony, est une interface qui permet aux collectionneurs de mangas de mettre leurs mangas préférés en valeur, en les partageant avec d'autres utilisateurs, les ajoutant dans leur collections.
+
 Ce site comporte plusieures entités:
 * Les mangas. Ils ont un id, nom et auteur.
 * L'inventaire, appelé bookshelf dans ce projet. Il a un id et un nom.
@@ -8,3 +9,8 @@ Ce site comporte plusieures entités:
 * L'utilisateur appelé user. Un utilisateur a un id, username, mot de passe (mdp) et email.
 
 Tous les attributs des entités mentionnés sont des string par défaut.
+
+Les routes accessibles dans ce site sont : 
+* http://127.0.0.1:8000/mangashelf: liste tous les mangashelfs crées
+* http://127.0.0.1:8000/mangashelf/{id}: affiche les informations d'un mangashelf spécifique
+* http://127.0.0.1:8000/manga/{id}: affiche les informations d'un manga spécifique
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -16,37 +16,37 @@ class AppFixtures extends Fixture
     {
         $faker = Factory::create();
 
-        // Création de Mangashelf
+        //Mangashelf
         $mangashelves = [];
         for ($i = 0; $i < 10; $i++) {
             $mangashelf = new Mangashelf();
             $mangashelf->setName($faker->word . ' Shelf'); 
 
             $manager->persist($mangashelf);
-            $mangashelves[] = $mangashelf;  // Sauvegarder les étagères pour les utiliser plus tard
+            $mangashelves[] = $mangashelf;
         }
 
-        // Création de Manga et association à Mangashelf
+        //Manga
         for ($i = 0; $i < 10; $i++) {
             $manga = new Manga();
             $manga->setName($faker->word); 
             $manga->setAuthor($faker->name); 
 
-            // Associer chaque Manga à un Mangashelf aléatoire
+            //Association de chaque Manga à un Mangashelf aléatoire
             $randomMangashelf = $mangashelves[array_rand($mangashelves)];
             $manga->setMangashelf($randomMangashelf);
 
             $manager->persist($manga);
         }
 
-        // Création de Mangatheque
+        //Mangatheque
         for ($i = 0; $i < 5; $i++) {
             $mangatheque = new Mangatheque();
             
             $manager->persist($mangatheque);
         }
 
-        // Création de User
+        //User
         for ($i = 0; $i < 10; $i++) {
             $user = new User();
             $user->setUsername($faker->userName); 
