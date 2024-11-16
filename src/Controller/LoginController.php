22fc0b9134eb3ac819a46cd2153c0912cdf06ed0
IlefRjiba <ilef.rjiba@insat.ucar.tn>
2024-11-16 10:01:22 +0100

    login added

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'app_login')]
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        if ($error) {
+            $this->addFlash('error', $error->getMessage());
+        }
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+        return $this->render('login/index.html.twig', [
+            'last_username' => $lastUsername,
+            // 'error'         => $error,
+        ]);
+    }
+
+    #[Route('/redirect-after-login', name: 'redirect_after_login')]
+    public function redirectAfterLogin(UserInterface $user): RedirectResponse
+    {
+        // Redirect to the member's profile page
+        return $this->redirectToRoute('app_member_show', ['id' => $user->getId()]);
+    }
+
+    #[Route('/logout', name: 'app_logout', methods: ['GET', 'POST'])]
+    public function logout()
+    {
+        // controller can be blank: it will never be called!
+        dump("logout");
+        // throw new \Exception('Don\'t forget to activate logout in security.yaml');
+        return new Response();
+    }
+}
